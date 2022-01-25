2df25f188531081a740bdda077662b2dd5dbbf77
Alan Cruz <alanchis@hotmail.com>
2022-01-25 17:24:49 -0600

    se agregan disparos, se agregan vidas, se agrega forma de perder

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
+
+//CONEXION AL CANVAS
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const buttonStart = document.getElementById("start");
 
 
+
+
+
+// VARIABLES GLOBALES
 let frames = 0;
 let requestID;
 let audio = new Audio ()
 audio.src = "assets/audio/pop.mp3"
 let audio1 = new Audio ()
 audio1.src ="assets/audio/gameover.mp3"
-let bullets =[]
+audio1.loop = false
+
 const enemies =[]
-const imageEnemies = ["assets/images/momia_v1.png","assets/images/momia_v2.png","assets/images/momiablanca_v1.png","assets/images/momiablanca_v2.png"]
+const imageEnemies = ["assets/images/momia_v1.png","assets/images/momia_v2.png","assets/images/mummy3.png","assets/images/mummy4.png","assets/images/mummy5.png"]
+
+const sillas = []
+const sillaImage = new Image()
+sillaImage.src = "assets/images/sillazo.png"
 
 
 // CLASES
-
 class Background{
     constructor(){
      
@@ -25,109 +35,134 @@ class Background{
         this.height = canvas.height;
         this.image = new Image();
         this.image.src = "assets/images/background3.png"
+        
     }   
 
-    //metodos
-
     draw() {
-        this.x -= 2;
+        this.x -= 1;
         if(this.x < -canvas.width){
             this.x = 0
         }
-
-
         ctx.drawImage(this.image,this.x,this.y, this.width,this.height)
-        
         ctx.drawImage(
             this.image,
             this.x + this.width, 
             this.y ,
             this.width,
-            this.height    
-            )
+            this.height)    
+        }
+    
+    gameOver(){
+        ctx.fillStyle="white"
+        ctx.font = "100px Arial"
+        ctx.fillText("GAME OVER",150,200)
+        audio1.play()
+        
+        
+        
+}}
+
+class Character {
+    constructor(x,y,w,h,img){
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.image = new Image()
+        this.image.src = img
     }
 
-    gameOver(){
-        ctx.fillStyle="red"
-        ctx.font = "70px Arial"
-        ctx.fillText("GAME OVER",50,325)
-        audio1.play()
-      }
-
-
-    
+    draw(){
+        if(frames % 10 === 0) {
+            this.x -= 10;
+        }
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+    collision(object) {
+        return (
+            this.x < object.x + object.width &&
+            this.x + this.width > object.x &&
+            this.y < object.y + object.height &&
+            this.y + this.height > object.y
+        )
+    }
 }
 
-class Luchador{
+
+class Luchador extends Character{
     constructor(x,y,w,h){
-      this.x = x;
-      this.y = y;
-      this.width = w;
-      this.height = h;
+        super(x, y, w, h)
       this.image = new Image()
       this.image.src = "assets/images/luchador.png"  
+      this.vidas = 5
     }
 
-  
-    //metodos
     draw(){
       ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
     }
+
+
+
   }
 
 
-  class Enemy{
-    constructor(x,y,w,h){
-      this.x = x;
-      this.y = y;
-      this.width = w;
-      this.height = h;
-      this.image = new Image()
-      this.image.src = ""
+class Enemy extends Character{
+    constructor(x,y,w,h, img){
+        super(x, y, w, h, img)
+      }
     }
-
   
-    //metodos
-    draw(){
-      ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+
+class Item {
+    constructor(w, h, img, positionX, positionY, speedX, speedY) {
+        this.width = w;
+        this.height = h;
+        this.image = new Image();
+        this.image.src = img;
+        this.position = {
+            x: positionX,
+            y: positionY
+        };
+        this.speed = {
+            x: speedX,
+            y: speedY
+        };
     }
-  }
-
-
-
-class Silla {
-    constructor(x, y){
-        this.x = x
-        this.y = y
-        this.width = 50
-        this.height = 50
-        this.image = new Image()
-        this.image.src = "assets/images/silla.png"
-    }
-
-    draw(){
-        
-        if(frames % 10 === 0){
-            this.x +=5
+    draw() {
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
         }
-        
-        ctx.drawImage(this.image,this.x,this.y, this.width, this.height)
+        collision(object) {
+            return (
+                this.position.x < object.x + object.width &&
+                this.position.x + this.width > object.x &&
+                this.position.y < object.y + object.height &&
+                this.position.y + this.height > object.y
+            )
+        }
     }
-
-
-}
-
-
-
+    
+    class Silla extends Item {
+        constructor(width, height, img, positionX, positionY, speedX, speedY) {
+            super(width, height, img, positionX, positionY, speedX, speedY);
+        }
+        draw() {
+            this.position.x += this.speed.x;
+            this.position.y += this.speed.y;
+            ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+        }
+    }
 
 
 // INSTANCIAS
-
 const background = new Background()
-const luchador = new Luchador(100,450,90,120)
-const silla = new Silla()
+const luchador = new Luchador(100,canvas.height/2,40,80)
 
 
+
+
+// START GAME
 function startGame() {
     console.log("prueba de inicio de juego")
     requestID = requestAnimationFrame(updateCanvas)
@@ -135,20 +170,18 @@ function startGame() {
 
 
 
-
-
-
-
+//UPDATE CANVAS
 function updateCanvas(){
     frames ++;
-  
-    ctx.clearRect(0,0,canvas.width, canvas.height)
+      ctx.clearRect(0,0,canvas.width, canvas.height)
     background.draw()
     luchador.draw()
     score()
     corazones()
     generateEnemies()
     drawEnemies()
+    lanzarSillas()
+    statusCheck()
     
 
 if(requestID){
@@ -159,124 +192,147 @@ if(requestID){
 
 
 
-// iniciar juego con un boton
-
+// INICIAR JUEGO CON UN CLICK DE BOTON
 buttonStart.addEventListener("click",startGame)
 
 
-
-
-
-
-// movimientos de 'el santo'
-addEventListener("keydown",(event) => {
-    //izquierda (flecha)
-    if(event.keyCode === 37){
-        if (luchador.x > 10){
-          luchador.x -=20  
-        }}
-    //derecha (flecha)
-    if(event.keyCode === 39){
-        if (luchador.x < canvas.width-80){
-          luchador.x +=20  
-        }}
-    //arriba (flecha)
-    if(event.keyCode === 38){
-        if (luchador.y > 70){
-          luchador.y -=20  
-        }}
-  
-        //abajo (flecha)
-        if(event.keyCode === 40){
-            if (luchador.y < canvas.height-120){
-              luchador.y +=20  
-            }}
-
-    //disparar (space)
-    if (event.keyCode === 32){
-            console.log("se dispara")
-            bullets.push(new Silla(luchador.x, luchador.y))
-            audio.play()
-        }
-    })
-
-// agregarle el conteo de score conforme pasa el tiempo
-function score(){
-    const puntos = Math.round(frames/25)
-    ctx.fillStyle="white"
-    ctx.font = '50px Arial';
-    ctx.fillText(`Score ${puntos}`, 1150, 65);
-  }
-
-// dibujarle un corazoncito con las vidas restantes
-  function corazones(){
-    const vida = new Image()
-    vida.src = "assets/images/vidas.png"
-    ctx.drawImage(vida,0,0,120,75)
-    ctx.fillStyle="white"
-    ctx.font = '50px Arial';
-    ctx.fillText(`X 10`, 110,60); // hay que meterle un if o un switch para que vayan bajando las vidas
-
-  }
-
-
-
-
-
-
-
-  // Generar enemigos
+  // GENERAR ENEMIGOS
   function generateEnemies(){
    
-    if(frames % 50===0 || frames % 100 === 0 || frames % 20 ===0){
+    if(frames % 370===0 || frames % 200 === 0 ){
 
-        let y = Math.floor(Math.random() * (1000 - 10) ) + 10
+        let y = Math.floor(Math.random() * (410 - 50) ) + 15
         let imgRand = Math.floor(Math.random() * imageEnemies.length)
-        const enemy = new Enemy (canvas.width,y,50,50,imageEnemies[imgRand])
+        const enemy = new Enemy (canvas.width,y,60,70,imageEnemies[imgRand])
         enemies.push(enemy)
-    }
-}
+    
+    }}
 
-// Pintar enemigos
+
+
+
+// DIBUJAR ENEMIGOS
 function drawEnemies(){
 
     enemies.forEach((enemy,index_enemy) => {
         enemy.draw()
-    })}
-//         //hacer collision
-//             // if(mario.collision(enemy)){
-//         //console.log("Me esta tocando")
-//         // requestID = undefined
-//         // fondo.gameOver()
-//         // }
-//         bullets.forEach((bullet,index_bullet) =>{
-//             bullet.draw()
-            
-//             //validar si choca con un enemigo
-//             if(enemy.collision(bullet)){
-//                 enemies.splice(index_enemy,1)
-//                 /*
-//                 if (enemy.type === 2){
-//                     score += 100
-//                 }              
 
-//                 */
-//             }
+        if(luchador.collision(enemy)){
+            console.log("hay contacto")
+            luchador.vidas --
+            enemies.splice(index_enemy,1)
+        }
 
-//             //sacar la bala si se sale del canvas
-//             if(bullet.x + bullet.width >= 800){
-//                 bullets.splice(index_bullet,1)
-//             }
-//         })
-
-//         //eliminar al enemigo si se sale del canvas
-//         //para sacar al enemigo y evitar que el browser se alente y se coma toda la ram
-//         //vamos a limpiar el array de los enemigos que ya no vemos en el canvas
-//         if (enemy.x + enemy.width <= 0){
-//             enemies.splice(index_enemy,1)
-//         }
+    sillas.forEach((silla,index_silla) => {
+        silla.draw()
+    
+        if(silla.collision(enemy)){
+            console.log("se muere una momia")
+            enemies.splice(index_enemy,1)
+            sillas.splice(index_silla,1)
+            }})
 
 
-//     });
-// }
+        if (enemy.x + enemy.width <= 0){
+            enemies.splice(index_enemy,1)
+            luchador.vidas --
+            console.log(enemies)
+            }
 
+        })}
+
+
+
+
+
+    
+
+
+            //sacar la bala si se sale del canvas
+            // if(silla.x + silla.width >= 800){
+            //     sillas.splice(index_silla,1)
+            // }
+        
+    
+
+
+
+
+
+//SCORE
+function score(){
+    const puntos = Math.round(frames/25)
+    ctx.fillStyle="white"
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score ${puntos}`, canvas.width-100, 25);
+  }
+
+
+
+//CORAZON DE VIDAS
+  function corazones(){
+    const vida = new Image()
+    vida.src = "assets/images/vidas.png"
+    ctx.drawImage(vida,0,5,60,30)
+    ctx.fillStyle="white"
+    ctx.font = '20 px Arial';
+    ctx.fillText(`X ${luchador.vidas}`, 60,30); // hay que meterle un if o un switch para que vayan bajando las vidas
+  }
+
+
+// STATUS
+function statusCheck() {
+    if (luchador.vidas < 0 ) {
+        background.gameOver()
+        requestAnimationFrame = null
+  }}
+
+
+
+
+
+
+
+// MOVIENTOS DEL SANTO
+addEventListener("keydown",(event) => {
+            //izquierda (flecha)
+    if(event.keyCode === 37){
+        if (luchador.x > 10){
+          luchador.x -=20  
+        }}
+            //derecha (flecha)
+    if(event.keyCode === 39){
+        if (luchador.x < canvas.width-luchador.width){
+          luchador.x +=20  
+        }}
+            //arriba (flecha)
+    if(event.keyCode === 38){
+        if (luchador.y > 20){
+          luchador.y -=20  
+        }}
+  
+            //abajo (flecha)
+        if(event.keyCode === 40){
+            if (luchador.y < canvas.height-luchador.height-15){
+              luchador.y +=20  
+            }}
+
+            //disparar (space)
+    if (event.keyCode === 32){
+            console.log("dispara una silla")
+            sillas.push(new Silla(40,40,sillaImage.src,luchador.x, luchador.y+25,3,0))
+            audio.play()
+        }
+    })
+
+   
+
+// DISPARAR SILLAZOS
+    function lanzarSillas() {
+        sillas.forEach((silla, silla_index) => {
+            silla.draw();
+            if (silla.position.y + silla.height <= 0) {
+                sillas.splice(silla_index, 1);
+            }
+        });
+    }
